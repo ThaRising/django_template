bb5ba6905945e367939a1481ad91c8948444124e
ThaRising <kochbe.ber@gmail.com>
2020-07-17 19:20:08 +0200

    Added OpenAPI documentation

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from .docs.openapi import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'),
+    url(r'^redoc/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
