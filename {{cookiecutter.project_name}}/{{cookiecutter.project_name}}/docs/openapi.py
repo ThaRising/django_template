230ee5f85f25fec163c15554ab874052aecf336c
ThaRising <kochbe.ber@gmail.com>
2020-07-17 22:04:33 +0200

    Fixed OpenAPI presets to incorporate user specified values

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/docs/openapi.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/docs/openapi.py
@@ -1,13 +1,14 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from .. import __version__
 
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
+      title="{{cookiecutter.project_name}} API",
+      default_version=__version__,
+      description="",
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
