b05aecb04975a1567d8136a0da86ba805ff087b7
ThaRising <kochbe.ber@gmail.com>
2020-09-03 12:15:41 +0200

    Added favicon view

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
@@ -1,24 +1,27 @@
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .docs.openapi import schema_view
 from django.conf import settings
+from django.views.generic.base import RedirectView
 from .apps.users.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
 urlpatterns = [
     # Included URL paths
     path('admin/', admin.site.urls),
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0),
-        name='schema-json'),
-    url(r'^redoc/$',
-        schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
-    url(r'^api-auth/', include('rest_framework.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
+    re_path(r'^redoc/$',
+            schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
+    re_path(r'^api-auth/', include('rest_framework.urls')),
+    re_path(r'^favicon\.ico$', favicon_view),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
