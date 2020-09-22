2848fb5ac37621cc0efc6c874fd747a8ec0ec3bd
ThaRising <kochbe.ber@gmail.com>
2020-09-22 11:32:34 +0200

    Added Sortable Admin site functionality

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/admin.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from .lib.admin import SortableAdminMixin
 
 
-class SiteAdmin(admin.AdminSite):
+class SiteAdmin(SortableAdminMixin, admin.AdminSite):
     site_header = "{{cookiecutter.project_name}} Administration"
     index_title = "{{cookiecutter.project_name}} Administration"
     site_title = "Administration"
+
+    admin_app_ordering = {
+        "auth": ("users", "auth"),
+    }
