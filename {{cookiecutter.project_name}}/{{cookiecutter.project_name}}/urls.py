7a0bed5f86a2c5c6b85326997e67e3e3d3be7565
ThaRising <kochbe.ber@gmail.com>
2020-11-17 10:21:56 +0100

    Changed URL mapping to a RESTful scheme

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
@@ -23,9 +23,9 @@ urlpatterns = [
     re_path(r'^api-auth/', include('rest_framework.urls')),
     re_path(r'^favicon\.ico$', favicon_view),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('tokens/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('tokens/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('tokens/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
     # User defined URL paths
 ]
