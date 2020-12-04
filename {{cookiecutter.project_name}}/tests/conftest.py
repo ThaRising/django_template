346be3d1f0a06857633ee0c544b6507c62781b70
ThaRising <kochbe.ber@gmail.com>
2020-12-04 16:51:47 +0100

    Added default tests for tokens and shared testing tools

---
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -0,0 +1,44 @@
+from typing import Dict, Tuple, Optional
+
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+# Default set of credentials for the test user
+TEST_USER_EMAIL = "default_tester@tester.de"
+TEST_USER_PASSWORD = "shittySecurity420!"
+
+
+def create_test_user(email: Optional[str] = None,
+                     password: Optional[str] = None):
+    User = get_user_model()
+    user = User.objects.create_user(
+        email=email or TEST_USER_EMAIL,
+        password=password or TEST_USER_PASSWORD
+    )
+    return user
+
+
+def obtain_tokens(cls: APITestCase,
+                  credentials: Dict[str, str]
+                  ) -> Tuple[str, str, Response]:
+    """ Attempts to obtain a token pair for a set of credentials """
+    url = reverse("tokens:obtain_delete")
+    res = cls.client.post(url, data=credentials)
+    if not res.status_code == status.HTTP_200_OK:
+        raise ValueError(
+            "Credentials invalid"
+        )
+
+    content = res.json()
+    access_token = content.get("access")
+    refresh_token = content.get("refresh")
+    return access_token, refresh_token, res
+
+
+__all__ = [
+    "TEST_USER_EMAIL", "TEST_USER_PASSWORD",
+    "create_test_user", "obtain_tokens"
+]
